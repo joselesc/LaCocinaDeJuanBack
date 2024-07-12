5da23aaa900312598e2ebe4d3ad120bc13cbc159
joselesc <joselescanogm@gmail.com>
2024-07-11 22:05:30 -0300

    Update lacocinadejuan.sql

--- lacocinadejuan.sql
+++ lacocinadejuan.sql
@@ -14,13 +14,29 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla lacocinadejuan.bebidas: ~6 rows (aproximadamente)
+INSERT INTO `bebidas` (`id_bebida`, `nombre`, `imagen`, `descripcion`, `precio`, `disponible`, `cantidad`) VALUES
+	(1, 'Coca-Cola', 'https://speedyburger.lecker123.de/media/image/1e/49/56/AdobeStock_284656005_Editorial_Use_Only_comp_600x600.jpg', 'Bebida gaseosa', 1000.00, 1, 50),
+	(2, 'Jugo de Naranja', 'https://www.65ymas.com/uploads/s1/58/99/46/bigstock-fresh-orange-juice-in-the-glas-407350976.jpeg', 'Jugo de naranja natural', 1200.00, 1, 30),
+	(3, 'Café', 'https://wallup.net/wp-content/uploads/2017/11/17/239445-coffee-coffee_beans-cup.jpg', 'Café negro', 2500.00, 1, 40),
+	(4, 'Té Verde', 'https://unareceta.com/wp-content/uploads/2017/02/te-verde.jpg', 'Té verde caliente', 1500.00, 1, 25),
+	(5, 'Cerveza', 'https://dam.ngenespanol.com/wp-content/uploads/2019/08/tipos-de-cerveza-2.jpg', 'Cerveza artesanal', 3000.00, 1, 60),
+	(6, 'Agua Mineral', 'https://parkingnearairports.io/img/3/68e694d17f-shutterstock249896590.jpg', 'Agua mineral sin gas', 1000.00, 1, 70);
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla lacocinadejuan.clientes: ~1 rows (aproximadamente)
+INSERT INTO `clientes` (`id_cliente`, `nombre`, `apellido`, `domicilio`, `telefono`, `mail`, `contraseña`) VALUES
+	(1, 'mail@mail.com', 'jorge', 'lopez', '802255', '225547788', 'hola');
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla lacocinadejuan.comidas: ~6 rows (aproximadamente)
+INSERT INTO `comidas` (`id_comida`, `nombre`, `imagen`, `descripcion`, `precio`, `disponible`, `cantidad`) VALUES
+	(1, 'Pizza Margherita', 'https://homemadehooplah.com/wp-content/uploads/2022/05/Classic-Margherita-Pizza-1-1200.jpg', 'Tomate, mozzarella y albahaca', 4000.00, 1, 10),
+	(2, 'Ensalada César', 'https://noro.mx/wp-content/uploads/2021/07/ensalada-cesar-origen.jpg', 'Lechuga, crutones, queso parmesano y aderezo César', 2500.00, 1, 20),
+	(3, 'Hamburguesa', 'https://amebeef.com/wp-content/uploads/2023/12/Carne-para-hamburguesa-Rica-Burger-80g-American-Beef.jpg', 'Carne, queso, lechuga, tomate y cebolla', 5000.00, 1, 15),
+	(4, 'Sushi', 'https://m2.live-resto.fr/files/establishments/cotesushirestaurantjaponaispruvienmarseillejoliettepradosaintbarnabelivraisonreservationemportersushimakicaliforniachirashi130021300813012_1510673214.jpg', 'Variedad de sushi', 9000.00, 1, 25),
+	(5, 'Tacos', 'https://www.cuisinart.com/globalassets/catalog/appliances/food-processors/complete-chef-cooking-food-processor/tacosamericanos_ss1262600101.jpg', 'Tortillas de maíz rellenas de carne y vegetales', 4000.00, 1, 30),
+	(6, 'Pasta Carbonara', 'https://www.supermomix.com/wp-content/uploads/2018/03/diary-free-carbonara.jpg', 'Pasta con salsa carbonara', 4500.00, 1, 18);
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla lacocinadejuan.pedidos: ~0 rows (aproximadamente)
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
